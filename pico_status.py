05ef49092a667ad5ed12d88ac3df77f825850f7f
Matthew Wire <devel@mrwire.co.uk>
2016-02-20 16:09:32 +0000

    Add watchdog and charging state

--- pico_status.py
+++ pico_status.py
@@ -15,6 +15,7 @@ import datetime
 i2c = smbus.SMBus(1)
 
 def pwr_mode():
+   time.sleep(0.1)
    data = i2c.read_byte_data(0x69, 0x00)
    data = data & ~(1 << 7)
    if (data == 1):
@@ -35,6 +36,23 @@ def rpi_level():
    data = i2c.read_word_data(0x69, 0x03)
    data = format(data,"02x")
    return (float(data) / 100)
+
+def watchdog_val():
+   time.sleep(0.1)
+   data = i2c.read_word_data(0x69, 0x0e)
+   data = format(data,"02x")
+   return data
+
+def charger_state():
+   time.sleep(0.1)
+   data = i2c.read_byte_data(0x69, 0x10)
+   data = data & ~(1 << 7)
+   if (data == 1):
+      return "CHARGING"
+   elif (data == 0):
+      return "NOT CHARGING"
+   else:
+      return "ERR"
 
 def fw_version():
    time.sleep(0.1)
@@ -70,11 +88,13 @@ def status():
     status = "TIME     : %s" % datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S') \
     + "\nFIRMWARE : %s" % fw_version() \
     + "\nSTATUS   : %s " % pwr_mode() \
+    + "\nSTATE    : %s " % charger_state() \
     + "\nBATTV    : %.02f Volts" % bat_level() \
     + "\nRPIV     : %.02f Volts" % rpi_level() \
     + "\nITEMP    : %s C" % sot23_temp() \
     + "\nA/D1 Voltage : %.02f V" % ad1_read() \
-    + "\nA/D2 Voltage : %.02f V" % ad2_read()
+    + "\nA/D2 Voltage : %.02f V" % ad2_read() \
+    + "\nWatchdog : %s" % watchdog_val()
     return status
 
 if __name__ == '__main__':
